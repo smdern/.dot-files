a9680c0404d95f9bd2c739a365f026fdf876bb5c
Shaun Dern <shaun@substantial.com>
2012-04-12 20:45:44 -0700

    adding hgrc

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+echo 'Symlinking dotfiles'
+
+ln -s ~/.dot-files/ackrc ~/.ackrc
+ln -s ~/.dot-files/bash_profile ~/.bash_profile
+ln -s ~/.dot-files/gitconfig ~/.gitconfig
+ln -s ~/.dot-files/gvimrc.local ~/.gvimrc.local
+ln -s ~/.dot-files/hgrc ~/.hgrc
