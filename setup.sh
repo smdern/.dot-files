a38489468ad773bda4ceb924a8a12ee7bde224bd
Shaun Dern <shaun@substantial.com>
2012-04-25 11:03:20 -0700

    adding gemrc

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s ~/.dot-files/bash_profile ~/.bash_profile
 ln -s ~/.dot-files/gitconfig ~/.gitconfig
 ln -s ~/.dot-files/gvimrc.local ~/.gvimrc.local
 ln -s ~/.dot-files/hgrc ~/.hgrc
+ln -s ~/.dot-files/gemrc ~/.gemrc
