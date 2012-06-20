9118905507c043c4dafb89cb50442b49b7082974
Shaun Dern <shaun@substantial.com>
2012-06-20 13:44:23 -0700

    using a vimrc.local

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo 'Symlinking dotfiles'
 ln -s ~/.dot-files/ackrc ~/.ackrc
 ln -s ~/.dot-files/bash_profile ~/.bash_profile
 ln -s ~/.dot-files/gitconfig ~/.gitconfig
-ln -s ~/.dot-files/gvimrc.local ~/.gvimrc.local
+ln -s ~/.dot-files/vimrc.local ~/.vimrc.local
 ln -s ~/.dot-files/hgrc ~/.hgrc
 ln -s ~/.dot-files/gemrc ~/.gemrc
 ln -s ~/.dot-files/tmux.conf ~/.tmux.conf
